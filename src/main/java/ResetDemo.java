642077f8d9162f431072b95b745e0982bab51193
Tamara <tamara_margova@abv.bg>
2025-07-20 12:06:48 +0300

    Version 1.0.4 with Mixed

--- src/main/java/ResetDemo.java
+++ src/main/java/ResetDemo.java
@@ -7,6 +7,7 @@ public class ResetDemo {
         System.out.println("Version 1.0.4 --> Hello World");
 
         System.out.println("Version 1.0.4");
+        System.out.println("Version 1.0.4--> Mixed");
 
 
     }
